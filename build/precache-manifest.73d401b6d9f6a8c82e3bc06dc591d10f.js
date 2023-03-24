0ae8afcc22c80036c619c8eae989970d6b37e2c1
theakhilnegi <akhilnegi3111@gmail.com>
2023-03-25 05:12:01 +0530

    edited

---
+++ build/precache-manifest.73d401b6d9f6a8c82e3bc06dc591d10f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbde864bf129082d778968033f3848a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "027e6d3e05a441bcb002",
+    "url": "/static/js/main.71662903.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
